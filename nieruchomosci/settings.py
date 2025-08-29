8199cf3d23aa1b2e531c381f90753b2825de4c0d
Karol Budzyn <kaponers@macbookair.home>
2025-08-29 13:49:50 +0200

    Dodanie hostow

--- nieruchomosci/settings.py
+++ nieruchomosci/settings.py
@@ -33,7 +33,8 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "dev-secret-key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["https://web-production-48d26.up.railway.app"]
+ALLOWED_HOSTS = ['.railway.app', '127.0.0.1', 'localhost']
+
 
 import dj_database_url
 
