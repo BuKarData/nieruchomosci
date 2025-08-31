8c696db44fb7603ae7f764551079ab973f93d4ea
Karol Budzyn <kaponers@Raccoons-MacBook.local>
2025-08-31 23:45:26 +0200

    fix

--- oferty/admin.py
+++ oferty/admin.py
@@ -1,23 +1,16 @@
 from django.contrib import admin
 from .models import Oferta, Inwestycja
 
-class CenaInline(admin.TabularInline):
-    model = Cena
-    extra = 1
-
-@admin.register(Oferta)
-class OfertaAdmin(admin.ModelAdmin):
-    list_display = ('inwestycja', 'adres', 'status', 'ostatnia_cena_display')
-    list_filter = ('status', 'inwestycja')
-    search_fields = ('adres',)
-    inlines = [CenaInline]
-
-    def ostatnia_cena_display(self, obj):
-        ostatnia_cena = obj.ceny.order_by('-data').first()
-        return f"{ostatnia_cena.kwota} zł" if ostatnia_cena else "Brak"
-    ostatnia_cena_display.short_description = 'Aktualna cena'
-
-
+# Rejestracja modelu Inwestycja
 @admin.register(Inwestycja)
 class InwestycjaAdmin(admin.ModelAdmin):
-    list_display = ('nazwa', 'opis', 'zdjecie')
+    list_display = ('nazwa', 'adres', 'data_dodania')
+    search_fields = ('nazwa', 'adres')
+    list_filter = ('data_dodania',)
+
+# Rejestracja modelu Oferta
+@admin.register(Oferta)
+class OfertaAdmin(admin.ModelAdmin):
+    list_display = ('adres', 'metraz', 'pokoje', 'status', 'inwestycja')
+    search_fields = ('adres',)
+    list_filter = ('status', 'inwestycja')
