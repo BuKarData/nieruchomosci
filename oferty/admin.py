c452caccce30461bace61db66d4d977ab1bc72be
Karol Budzyn <kaponers@Raccoons-MacBook.local>
2025-08-31 22:35:24 +0200

    fix

--- oferty/admin.py
+++ oferty/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Oferta, Cena
+from .models import Oferta, Inwestycja
 
 admin.site.register(Oferta)
-admin.site.register(Cena)
+admin.site.register(Inwestycja)
--- oferty/forms.py
+++ oferty/forms.py
@@ -1,15 +1,15 @@
-from django import forms
+from django.contrib import admin
 from .models import Inwestycja, Oferta
 
-# -------- Formularz dla inwestycji --------
-class InwestycjaForm(forms.ModelForm):
-    class Meta:
-        model = Inwestycja
-        fields = ["nazwa", "opis", "zdjecie"]
+
+@admin.register(Inwestycja)
+class InwestycjaAdmin(admin.ModelAdmin):
+    list_display = ("nazwa", "opis", "zdjecie")
+    search_fields = ("nazwa",)
 
 
-# -------- Formularz dla oferty/lokalu --------
-class OfertaForm(forms.ModelForm):
-    class Meta:
-        model = Oferta
-        fields = ["inwestycja", "adres", "metraz", "cena", "status", "pokoje"]
+@admin.register(Oferta)
+class OfertaAdmin(admin.ModelAdmin):
+    list_display = ("adres", "metraz", "cena", "status", "pokoje", "inwestycja")
+    list_filter = ("status", "inwestycja")
+    search_fields = ("adres",)
