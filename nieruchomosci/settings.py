a2f0d32b88d0663881afe85a1b81cb68ffaa42a6
Karol Budzyn <kaponers@Raccoons-MacBook.local>
2025-09-01 00:56:50 +0200

    fix

--- nieruchomosci/settings.py
+++ nieruchomosci/settings.py
@@ -71,13 +71,21 @@ TEMPLATES = [
     }
 ]
 
-# Baza danych
+# weź URL z kilku możliwych nazw, bo na Railway bywa różnie
+_DB_URL = (
+    os.getenv("DATABASE_PUBLIC_URL")
+    or os.getenv("RAILWAY_DATABASE_URL")
+    or os.getenv("POSTGRES_URL")          # czasem plugin wystawia też taką zmienną
+)
+
+if not _DB_URL:
+    # WOLNO użyć SQLite TYLKO lokalnie. Na produkcji to skasuj!
+    _DB_URL = "sqlite:///db.sqlite3"
+
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_PUBLIC_URL'),
-        conn_max_age=600,
-    )
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
+
 
 # Walidatory haseł
 AUTH_PASSWORD_VALIDATORS = [
