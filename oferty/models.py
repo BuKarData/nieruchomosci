77f6eed394723c20fe35ed2f7b3fd0406a569ee9
Karol Budzyn <kaponers@Raccoons-MacBook.local>
2025-09-01 18:47:45 +0200

    fix

--- oferty/models.py
+++ oferty/models.py
@@ -6,7 +6,7 @@ class Inwestycja(models.Model):
     adres = models.CharField(max_length=300)
     data_dodania = models.DateTimeField(auto_now_add=True)
     zdjecie = models.ImageField(upload_to="inwestycje/", blank=True, null=True)
-
+    opis = models.TextField(blank=True, null=True)
     def __str__(self):
         return self.nazwa
 
