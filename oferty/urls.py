285010eab0202f947cd30956d3a19c4f5bcfed8b
Karol Budzyn <kaponers@Raccoons-MacBook.local>
2025-09-01 13:33:24 +0200

    fix

--- oferty/urls.py
+++ oferty/urls.py
@@ -4,11 +4,12 @@ from oferty import views
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", views.home, name="home"),  # strona główna
-    path("oferty/", include("oferty.urls")),
+    path('', views.home, name='home'),
+    path('oferty/', views.lista_ofert, name='lista_ofert'),
 ]
+
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
