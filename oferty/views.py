0b816a043bdf0770d7feaf688f924bba3a3449e0
Karol Budzyn <kaponers@Raccoons-MacBook.local>
2025-09-01 02:29:26 +0200

    fix

--- oferty/views.py
+++ oferty/views.py
@@ -3,7 +3,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 from django.db.models import Prefetch
 from .forms import OfertaForm, CenaForm
-from .models import Oferta, Cena
+from .models import Oferta, Cena, Inwestycja
 
 
 # Strona główna
