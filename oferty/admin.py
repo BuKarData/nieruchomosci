69569267e73ef633599abcf3c95901887b0f4aa4
Karol Budzyn <kaponers@Raccoons-MacBook.local>
2025-09-01 03:38:02 +0200

    fix

--- oferty/admin.py
+++ oferty/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from .models import Oferta, Cena, Inwestycja
 
-admin.site.register(Oferta)
-admin.site.register(Cena)
+#admin.site.register(Oferta)
+#admin.site.register(Cena)
 
 
 @admin.register(Inwestycja)
