5c433fec297852901e53f60479578a24d3451cfa
Karol Budzyn <kaponers@Raccoons-MacBook.local>
2025-09-01 03:36:37 +0200

    fix

--- oferty/admin.py
+++ oferty/admin.py
@@ -3,3 +3,22 @@ from .models import Oferta, Cena
 
 admin.site.register(Oferta)
 admin.site.register(Cena)
+
+
+@admin.register(Inwestycja)
+class InwestycjaAdmin(admin.ModelAdmin):
+    list_display = ("nazwa", "adres", "data_dodania")
+    search_fields = ("nazwa", "adres")
+
+
+@admin.register(Oferta)
+class OfertaAdmin(admin.ModelAdmin):
+    list_display = ("adres", "metraz", "pokoje", "status", "data_dodania")
+    list_filter = ("status",)
+    search_fields = ("adres",)
+
+
+@admin.register(Cena)
+class CenaAdmin(admin.ModelAdmin):
+    list_display = ("oferta", "kwota", "data")
+    list_filter = ("data",)
